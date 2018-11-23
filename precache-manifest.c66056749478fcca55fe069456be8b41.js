d3cb7cbd57af50ed9bcd251d4797a029754ff876
Zikun Fan <zifan@microsoft.com>
2018-11-23 16:38:43 +0800

    add functional settings

---
+++ precache-manifest.c66056749478fcca55fe069456be8b41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "607338c8122c8f3a89e466a0cd00306c",
+    "url": "/static/media/feedback_url_image.607338c8.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "28eb97621c1384ad23a1",
+    "url": "/static/js/main.28eb9762.chunk.js"
+  },
+  {
+    "revision": "8587bcdc2dee5f5c04a9",
+    "url": "/static/js/1.8587bcdc.chunk.js"
+  },
+  {
+    "revision": "28eb97621c1384ad23a1",
+    "url": "/static/css/main.b93820c8.chunk.css"
+  },
+  {
+    "revision": "8587bcdc2dee5f5c04a9",
+    "url": "/static/css/1.d4640487.chunk.css"
+  },
+  {
+    "revision": "30678c4d54995dc935e1ad8ef198542e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da1972fd53145bd6b290123e629ab769.js"
+  "/precache-manifest.c66056749478fcca55fe069456be8b41.js"
 );
 
 workbox.clientsClaim();
