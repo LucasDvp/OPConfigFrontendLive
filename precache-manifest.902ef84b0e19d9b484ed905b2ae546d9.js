254237543bb728720ff6ea165a8a799ffb93fbc7
Zikun Fan <zifan@microsoft.com>
2018-11-28 11:04:29 +0800

    update

---
+++ precache-manifest.902ef84b0e19d9b484ed905b2ae546d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b448b976192e3600de78",
+    "url": "/static/js/main.b448b976.chunk.js"
+  },
+  {
+    "revision": "b15c60cd73b91e232e9b",
+    "url": "/static/js/1.b15c60cd.chunk.js"
+  },
+  {
+    "revision": "b448b976192e3600de78",
+    "url": "/static/css/main.35a6df3c.chunk.css"
+  },
+  {
+    "revision": "b15c60cd73b91e232e9b",
+    "url": "/static/css/1.091d1b6d.chunk.css"
+  },
+  {
+    "revision": "4a69d172fbefec4583bd9da9138f12ce",
+    "url": "/index.html"
+  }
+];
